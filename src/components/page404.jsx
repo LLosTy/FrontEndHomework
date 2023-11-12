731bd6b7714aeea8bd7a995b1e9639d5244e2b62
LLosTy <marcojurgenjuras@gmail.com>
2023-11-12 18:27:37 +0100

    Tweaked navbar, added navbar to Error404

--- src/components/page404.jsx
+++ src/components/page404.jsx
@@ -1,11 +1,14 @@
+import Navbar from "./navbar";
 const Page404 = () => {
     return (
-        <div class="d-flex justify-content-center">
-            <div class="d-flex align-items-center">
-                <h1>Error 404</h1>
+        <div>
+            <Navbar></Navbar>
+            <div class="d-flex justify-content-center">
+                <div class="d-flex align-items-center">
+                    <h1>Error 404</h1>
+                </div>
             </div>
         </div>
-
     )
 }
 export default Page404;
